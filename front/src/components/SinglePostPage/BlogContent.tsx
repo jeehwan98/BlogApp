f2d15ccb6e0bdc6ac8fd32f4cf0eb8196fc36686
Jee Hwan <jeehwan98@naver.com>
2025-01-23 22:24:51 +0900

    refactor: single post page

---
+++ front/src/components/SinglePostPage/BlogContent.tsx
@@ -0,0 +1,9 @@
+export default function BlogContent() {
+  return (
+    <div className="max-w-4xl w-full mt-10">
+      <p className="">
+        Content
+      </p>
+    </div>
+  )
+}
---
+++ front/src/components/SinglePostPage/BlogTitle.tsx
@@ -0,0 +1,7 @@
+export default function BlogTitle() {
+  return (
+    <h1 className="lg:text-5xl md:text-4xl text-2xl font-bold">
+      2025년 합격하는 개발자 포트폴리오
+    </h1>
+  )
+}
