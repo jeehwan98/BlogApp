505901d2eccb6ff5427c840752707b62dfef18d4
Jee Hwan <jeehwan98@naver.com>
2025-01-23 23:14:30 +0900

    feat: comments

--- front/src/app/test/page.tsx
+++ front/src/app/test/page.tsx
@@ -1,5 +1,6 @@
 import { BlogPageWrapper } from "@/components/SinglePostPage/BlogComponents";
 import BlogPost from "@/components/SinglePostPage/BlogPost";
+import Comments from "@/components/SinglePostPage/Comments";
 import CommentSection from "@/components/SinglePostPage/CommentSection";
 
 export default function SinglePostPage() {
@@ -7,6 +8,10 @@ export default function SinglePostPage() {
     <BlogPageWrapper>
       <BlogPost />
       <CommentSection />
+      <Comments />
+      <Comments />
+      <Comments />
+      <Comments />
     </BlogPageWrapper>
   )
 }
