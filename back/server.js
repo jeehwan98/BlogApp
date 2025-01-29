81bc5d5b352251c46d852342dc6216045398f7b5
Jee Hwan <jeehwan98@naver.com>
2025-01-29 21:39:05 +0900

    feat: back

--- back/server.js
+++ back/server.js
@@ -3,17 +3,17 @@ const app = express();
 const PORT = 8080;
 
 app.get("/api/home", (req, res) => {
-  res.json({ message: "Hello World!" });
+    res.json({message: "Hello World!"});
 });
 
 console.log(process.env.test);
 
 app.get("test", (req, res) => {
-  res.status(200).send("it works!");
+    res.status(200).send("it works!");
 })
 
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
 
