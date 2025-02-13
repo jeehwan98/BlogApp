ea508bf7e7ed475ce12ba68b1810bc2b0ecc3656
Jee Hwan <jeehwan98@naver.com>
2025-02-13 14:06:14 +0900

    refactor: comment section

--- front/src/components/SinglePostPage/CommentSection.tsx
+++ front/src/components/SinglePostPage/CommentSection.tsx
@@ -1,16 +1,38 @@
+"use client"
+
+import { useRef, useState } from "react";
 import { Button } from "../UI/Button";
 
 export default function CommentSection() {
+  const [comment, setComment] = useState<string>("");
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = "auto"; // reset height
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`; // expand dynamically
+    }
+  };
+
   return (
     <div className="flex flex-col lg:gap-6 md:gap-5 gap-4 w-full mt-5">
-      <div className="flex gap-1">
-        <span className="font-semibold">0</span>
+      {/* COMMENT TITLE */}
+      <div className="flex items-center gap-1">
+        <span className="lg:text-xl md:text-lg text-base font-semibold">0</span>
         <h1 className="lg:text-xl md:text-lg text-base font-semibold">Comments</h1>
       </div>
+      {/* COMMENT WRITE AREA */}
       <div className="flex items-center justify-between gap-8">
         <textarea
-          className="w-full h-24 px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
+          ref={textAreaRef}
+          className="w-full min-h-[40px] max-h-[200px] px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
           placeholder="Write a comment..."
+          value={comment}
+          onChange={handleChange}
+          rows={3} // start with 3 rows
+          style={{ overflowY: "hidden", resize: "none" }} // prevent manual resize
         />
       </div>
       <div className="flex justify-end">
